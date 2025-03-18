20d8b09411e8f068e932896c0ab901a480b0757d
David Howden <dhowden@gmail.com>
2025-03-18 18:29:04 +1100

    add support for search_rules

--- cmd/mcp/main.go
+++ cmd/mcp/main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/mark3labs/mcp-go/mcp"
 	"github.com/mark3labs/mcp-go/server"
 
+	"github.com/algolia/algoliasearch-client-go/v3/algolia/opt"
 	"github.com/algolia/algoliasearch-client-go/v3/algolia/search"
 )
 
@@ -85,7 +86,9 @@ func main() {
 
 		var x map[string]any
 		if err := index.GetObject(objectID, &x); err != nil {
-			return nil, fmt.Errorf("could not get object: %w", err)
+			return mcp.NewToolResultError(
+				fmt.Sprintf("could not get object: %v", err),
+			), nil
 		}
 
 		b, err := json.Marshal(x)
@@ -94,6 +97,59 @@ func main() {
 		}
 
 		return mcp.NewToolResultResource("object", mcp.TextResourceContents{
+			MIMEType: "application/json",
+			Text:     string(b),
+		}), nil
+	})
+
+	searchRulesTool := mcp.NewTool(
+		"search_rules",
+		mcp.WithDescription("Search for rules in the Algolia index"),
+		mcp.WithString(
+			"query",
+			mcp.Description("The query to search for"),
+			mcp.Required(),
+		),
+		mcp.WithString(
+			"anchoring",
+			mcp.Description("When specified, restricts matches to rules with a specific anchoring type. When omitted, all anchoring types may match."),
+			mcp.Enum("is", "contains", "startsWith", "endsWith"),
+		),
+		mcp.WithString(
+			"context",
+			mcp.Description("When specified, restricts matches to contextual rules with a specific context. When omitted, all contexts may match."),
+		),
+		mcp.WithBoolean(
+			"enabled",
+			mcp.Description("When specified, restricts matches to rules with a specific enabled status. When omitted, all enabled statuses may match."),
+		),
+	)
+
+	mcps.AddTool(searchRulesTool, func(ctx context.Context, req mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+		query, _ := req.Params.Arguments["query"].(string)
+
+		opts := []any{}
+		if anchoring, ok := req.Params.Arguments["anchoring"].(string); ok {
+			opts = append(opts, opt.Anchoring(anchoring))
+		}
+		if context, ok := req.Params.Arguments["context"].(string); ok {
+			opts = append(opts, opt.RuleContexts(context))
+		}
+		if enabled, ok := req.Params.Arguments["enabled"].(bool); ok {
+			opts = append(opts, opt.EnableRules(enabled))
+		}
+
+		resp, err := index.SearchRules(query, opts...)
+		if err != nil {
+			return nil, fmt.Errorf("could not search rules: %w", err)
+		}
+
+		b, err := json.Marshal(resp)
+		if err != nil {
+			return nil, fmt.Errorf("could not marshal response: %w", err)
+		}
+
+		return mcp.NewToolResultResource("rules", mcp.TextResourceContents{
 			MIMEType: "application/json",
 			Text:     string(b),
 		}), nil
